5dc99b12c7c48d69e2d24773ee542b0865b53857
studentfaruk0308 <studentfaruk0308@gmail.com>
2022-03-21 21:23:52 +1100

    Dated21032022

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+function saturdayFun (activity ="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+  };
+
+  function mondayWork (activity ="go to the office") {
+    return `This Monday, I will ${activity}.`
+  };
+
+  function wrapAdjective(style="*"){
+    return function(value="special"){
+      return `You are ${style}${value}${style}!`
+    }
+  }
+
+  let Calculator = {
+    add: function (x,y){
+      return x+y
+    },
+    subtract: function (x,y){
+      return x-y
+    },
+    multiply: function(x,y){
+      return x*y
+    },
+    divide: function(x,y){
+      return x/y
+    }
+  }
+
+  function actionApplyer(start, arr){
+    for(let i=0; i<arr.length;i++){
+      start=arr[i](start);
+    }
+    return start
+  }
